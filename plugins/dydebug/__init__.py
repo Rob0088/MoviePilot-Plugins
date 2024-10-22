069a1afa031151baa129eb36fc07f0ac00d32e84
Rob0088 <prarame56@gmail.com>
2024-10-22 08:21:47 +0800

    Update __init__.py

--- plugins/dydebug/__init__.py
+++ plugins/dydebug/__init__.py
@@ -29,7 +29,7 @@ class Dydebug(_PluginBase):
     # 插件图标
     plugin_icon = "Wecom_A.png"
     # 插件版本
-    plugin_version = "0.1.0"
+    plugin_version = "0.1.1"
     # 插件作者
     plugin_author = "RamenRa"
     # 作者主页
