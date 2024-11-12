755e6aab82a745e4d89f36ee4ee418d44b904c6e
Rob0088 <prarame56@gmail.com>
2024-11-12 22:29:39 +0800

    Update notify_helper.py

--- plugins/dydebug/notify_helper.py
+++ plugins/dydebug/notify_helper.py
@@ -17,7 +17,6 @@ class MySender:
 
     def send_channel(self):
         if self.token:
-            # 优先判断是否为 WeChat 渠道
             if self.token == "WeChat":
                 return "WeChat"
 
@@ -32,7 +31,7 @@ class MySender:
         return None
 
     # 标题，内容，图片，是否强制发送
-    def send(self, title, content, image=None, force_send=False, diy_chnnel=None):
+    def send(self, title, content, image=None, force_send=False, diy_channel=None):
         if not self.init_success:
             return  # 如果初始化失败，直接返回
         # 判断发送的内容类型
@@ -43,50 +42,38 @@ class MySender:
                 return
             else:
                 self.first_text_sent = True
-        # 根据发送渠道调用相应的发送方法
-        if not diy_chnnel:  # 用户没有指定通知方式，使用初始化时确定的通知方式
-            if self.channel == "WeChat":
-                self.send_wechat(title, content, image)
-            elif self.channel == "ServerChan":
-                self.send_serverchan(title, content, image)
-            elif self.channel == "AnPush":
-                self.send_anpush(title, content, image)
-            elif self.channel == "PushPlus":
-                self.send_pushplus(title, content, image)
-            else:
-                raise ValueError("Unknown channel")
-        else:
-            if diy_chnnel == "WeChat":
-                self.send_wechat(title, content, image)
-            elif diy_chnnel == "ServerChan":
-                self.send_serverchan(title, content, image)
-            elif diy_chnnel == "AnPush":
-                self.send_anpush(title, content, image)
-            elif diy_chnnel == "PushPlus":
-                self.send_pushplus(title, content, image)
-            else:
-                raise ValueError("Unknown channel")
 
-    def send_wechat(self, title, content, contains_image):
-        # WeChat发送逻辑
+        try:
+            if not diy_channel:
+                channel = self.channel
+            else:
+                channel = diy_channel
+
+            if channel == "WeChat":
+                return self.send_wechat(title, content, image)
+            elif channel == "ServerChan":
+                return self.send_serverchan(title, content, image)
+            elif channel == "AnPush":
+                return self.send_anpush(title, content, image)
+            elif channel == "PushPlus":
+                return self.send_pushplus(title, content, image)
+            else:
+                return "Unknown channel"
+        except Exception as e:
+            return f"Error occurred: {str(e)}"
+
+    def send_wechat(self, title, content, image):
         wechat = WeChat()
-        if contains_image:
-            # 发送带图片的消息
-            send_status = wechat.send_msg(
-                title='企业微信登录二维码',
-                # text=f"二维码刷新时间：{content}",
-                image=contains_image,
-                link=contains_image
-            )
-        # WeChat().send_msg(title = "点击扫描二维码登录企业微信",image='。。。',link='。。。')
+        if image:
+            send_status = wechat.send_msg(title='企业微信登录二维码', image=image, link=image)
         else:
-            # 发送纯文本消息
-            send_status = wechat.send_msg(
-                title=title,
-                text=f"{content}"
-            )
+            send_status = wechat.send_msg(title=title, text=content)
 
-    def send_serverchan(self, title, content, contains_image):
+        if send_status is None:
+            return "微信通知发送错误"
+        return None
+
+    def send_serverchan(self, title, content, image):
         if self.token.startswith('sctp'):
             match = re.match(r'sctp(\d+)t', self.token)
             if match:
@@ -97,65 +84,48 @@ class MySender:
         else:
             url = f'https://sctapi.ftqq.com/{self.token}.send'
 
-        if contains_image:
-            params = {
-                'title': title,
-                'desp': f'![img]({contains_image})',
-            }
-        else:
-            params = {
-                'title': title,
-                'desp': f'{content}',
-                # **options
-            }
-        headers = {
-            'Content-Type': 'application/json;charset=utf-8'
-        }
+        params = {'title': title, 'desp': f'![img]({image})' if image else content}
+        headers = {'Content-Type': 'application/json;charset=utf-8'}
         response = requests.post(url, json=params, headers=headers)
         result = response.json()
+        if result.get('code') != 0:
+            return f"Server酱通知错误: {result.get('message')}"
+        return None
 
-    def send_anpush(self, title, content, contains_image):
+    def send_anpush(self, title, content, image):
         if ',' in self.token:
             channel, token = self.token.split(',', 1)
         else:
             return
         url = f"https://api.anpush.com/push/{token}"
-        # AnPush发送逻辑，带二维码的特殊处理
-        if contains_image:
-            payload = {
-                "title": title,
-                "content": f"<img src=\"{contains_image}\" width=\"100%\">",
-                "channel": channel
-            }
-        else:
-            payload = {
-                "title": title,
-                "content": f"{content}",
-                "channel": channel
-            }
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
+        payload = {
+            "title": title,
+            "content": f"<img src=\"{image}\" width=\"100%\">" if image else content,
+            "channel": channel
         }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
         response = requests.post(url, headers=headers, data=payload)
         result = response.json()
+        # 判断返回的code和msgIds
+        if result.get('code') != 200:
+            return f"AnPush: {result.get('msg')}"
+        elif not result.get('data') or not result['data'].get('msgIds'):
+            return "AnPush 消息通道未找到"
+        return None
 
-    def send_pushplus(self, title, content, contains_image):
+    def send_pushplus(self, title, content, image):
         pushplus_url = f"http://www.pushplus.plus/send/{self.token}"
         # PushPlus发送逻辑
-        if contains_image:
-            pushplus_data = {
-                "title": title,
-                "content": f"企业微信登录二维码<br/><img src='{contains_image}' />",
-                "template": "html"
-            }
-        else:
-            pushplus_data = {
-                "title": title,
-                "content": f"{content}",
-                "template": "html"
-            }
-        response = requests.post(pushplus_url, json=pushplus_data)
+        data = {
+            "title": title,
+            "content": f"企业微信登录二维码<br/><img src='{image}' />" if image else content,
+            "template": "html"
+        }
+        response = requests.post(pushplus_url, json=data)
         result = response.json()
+        if result.get('code') != 200:
+            return f"PushPlus send failed: {result.get('msg')}"
+        return None
 
     def reset_limit(self):
         """解除限制，允许再次发送纯文本消息"""
