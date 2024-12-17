a740bfae28d7c3f31a5d8d72d76a36d4618d140f
Rob0088 <prarame56@gmail.com>
2024-12-17 22:32:28 +0800

    Update __init__.py

--- plugins/dydebug/__init__.py
+++ plugins/dydebug/__init__.py
@@ -30,7 +30,7 @@ class Dydebug(_PluginBase):
     # 插件图标
     plugin_icon = "Wecom_A.png"
     # 插件版本
-    plugin_version = "1.7.2"
+    plugin_version = "1.7.3"
     # 插件作者
     plugin_author = "RamenRa"
     # 作者主页
@@ -313,7 +313,7 @@ class Dydebug(_PluginBase):
     def CheckIP(self):
         if self.wan2:
             ip_address = self.wan2.read_all_ips()
-            if not ip_address:
+            if ip_address == "获取IP失败":
                 time.sleep(7)
                 ip_address = self.wan2.read_all_ips()
             url = self.wan2_url
